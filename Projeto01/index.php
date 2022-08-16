98a27e6c7d9a334a8e6811e3c5422910a8fe9666
hrk77 <103191376+hrk77@users.noreply.github.com>
2022-08-16 09:39:09 -0300

    Add files via upload

---
+++ Projeto01/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Projeto 01</title>
+    <script src="https://kit.fontawesome.com/11fe6241e9.js" crossorigin="anonymous"></script>
+    <meta charset="utf-8">
+    <meta name="description" content="Pagina da republica popular da China"> <!-- para o google encontrar o site -->
+    <meta name="keywords" content="China, Republica, Popular, Xi Jinping, Economia, Tecnologia, Exercito">
+</head>
+<body>
+
+        <!-- iniciar a marcação do meu site -->
+        <header>
+            <div class="center">
+            <div class="logo left">Logo Marca</div> <!-- logo -->
+            <nav class="desktop right"> <!-- site sera responsivo -->
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+            <nav class="mobile right"> <!-- site será responsibo -->
+
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+        </div> <!-- center -->
+        </header>
+
+        <!-- banner -->
+        <section class="banner-principal">
+            <div class="overlay"></div>
+            <div class="center"></div>
+            <!-- formulario onde o usuario vai colocar o email -->
+
+            <form>]
+                <h2> Qual o seu melhor e-mail?</h2>
+                <input type="e-mail" name="e-mail" required/>
+                <input type="submit" name="acao" value="Cadastrar"/>
+            </form>    
+        </section>
+
+        <!-- descrição do autor -->
+        <section class="descrição-autor">
+            <div class="center">
+                <div class="w50 left"> <!-- vai pegar 50% da largura da tela -->
+                    <h2> Basato_Bigode</h2>
+                    <p>天上太阳红呀红彤彤诶心中的太阳是毛泽东诶他领导我们得解放诶人民翻身当家做主人咿呀咿吱呦喂呀而呀吱呦啊人民翻身当家做主人天上太阳红呀红彤彤诶心中的太阳是毛泽东诶他领导我们奋勇前进诶革命江山一片红诶咿呀咿吱呦喂呀而呀吱呦啊革命江山一片红天上太阳红呀红彤彤诶心中的太阳是毛泽东诶他领导我们奋勇前进诶革命江山一片红诶嗦啦啦子嗦啦啦子一呀一片红</p>
+                    <p>天上太阳红呀红彤彤诶
+心中的太阳是毛泽东诶
+他领导我们得解放诶
+人民翻身当家做主人
+咿呀咿吱呦喂呀而呀吱呦啊
+人民翻身当家做主人
+天上太阳红呀红彤彤诶
+心中的太阳是毛泽东诶
+他领导我们奋勇前进诶
+革命江山一片红诶
+咿呀咿吱呦喂呀而呀吱呦啊
+革命江山一片红
+天上太阳红呀红彤彤诶
+心中的太阳是毛泽东诶
+他领导我们奋勇前进诶
+革命江山一片红诶
+嗦啦啦子嗦啦啦子
+一呀一片红</p>
+                </div> <!-- fechou a w50 -->
+            </div>
+        </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
